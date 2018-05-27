092a64e57fb96c98f84e6e7bfafa39d3d11f52f7
Daniel Paz <contato@danielpaz.net>
2018-05-27 16:26:08 -0300

    authentication

--- js/autentication.js
+++ js/autentication.js
@@ -1,24 +1,37 @@
-var authEmailPassButton = document.getElementById('authEmailPassButton');
+
 var authGoogleButton = document.getElementById('authGoogleButton');
 
-var createUserButton = document.getElementById('createUserButton');
 var logoutButton = document.getElementById('logoutButton');
-
-var emailInput = document.getElementById('emailInput');
-var passwordInput = document.getElementById('passwordInput');
 
 var displayName = document.getElementById('displayName');
 
-createUserButton.addEventListener('click', function () {
+logoutButton.addEventListener('click', function () {
     firebase
-        .auth()
-        .createUserWithEmailAndPassword(emailInput.value, passwordInput.value)
+        .auth() 
+        .signOut()
         .then(function () {
-            alert('Bem vindo ' + emailInput.value);
-        })
-        .catch(function (error) {
-            console.error(error.code);
-            console.error(error.message);
-            alert('Falha ao cadastrar, verifique o erro no console: ' + error.message)
+            displayName.innerText = 'Você não está autenticado';
+            alert('Você saiu.');
+        }, function (error) {
+            console.error(error);
         });
 });
+
+authGoogleButton.addEventListener('click', function () {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    signIn(provider);
+});
+
+function signIn(provider) {
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then(function (result) {
+            console.log(result);
+            var token = result.credential.acessToken;
+            displayName.innerText = 'Bem vindo, ' + result.user.displayName;
+        }).catch(function (error) {
+            console.log(error);
+            alert('Fala na autenticação');
+        })
+};
+
